9c0934f5bcf54f348a53238b761f78bc0982eff1
Julien <julien.darras12@gmail.com>
2021-08-20 15:03:19 +0200

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,27 +37,67 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 let category = item.category
 
                 document.getElementById('accessories').innerHTML += `<div  class='col-md-3 col-lg-3 col-sm-6 border'><img src="${picture}"></img> ${productName}, ${price} euros  <br><input data-ref= ${reference} data-product = ${productName} data-price = ${price} type="image" class="addToCart" src="assets/img/addToCart.png"></div>`
+                createEvent()
             })
 
-let btnAddToCart = document.getElementsByClassName('addToCart') 
-for (let i = 0; i < btnAddToCart.length; i++) {
-btnAddToCart[i].addEventListener('click', addToCart)
-}
         })
+
 })
+// let cart = []
+// let btnAddToCart = document.getElementsByClassName('addToCart');
+// console.log(btnAddToCart)
+// for (let i = 0; i < btnAddToCart.length; i++) {
+//     btnAddToCart[i].onclick = (event) => {
 
-let cart = []
+//         let ref = event.target.dataset.ref;
+//         cart.push(ref)
+//         console.log(ref)
+//         console.log(cart)
+//     }
+// }
+var array = [];
+var modalCart = document.getElementById("modalCart")
+var articleInCart = []
+let createEvent = () => {
+    var btnAddToCart = document.querySelectorAll('.addToCart');
+    btnAddToCart.forEach(element => {
+        element.onclick = (event) => {
+            let ref = event.target.dataset.ref;
+            let product = event.target.dataset.product;
+            let productPrice = event.target.dataset.price;
+            let quantity = 1;
+            if(localStorage.getItem(ref)){
+                stock = JSON.parse(localStorage.getItem(ref))
+                stock[3]++;
+                console.log(stock);
+                localStorage.setItem(ref,JSON.stringify(stock))
+            }else{
+                array = [product, productPrice, ref, quantity];
+                localStorage.setItem(ref,JSON.stringify(array))
+            }
+            
+            
 
-function addToCart(name, id, price, count) {
-    let array = []
-    let product = this.dataset.product;
-    let ref = this.dataset.ref
-    let productPrice = this.dataset.price;
-    array.push(product, ref, productPrice)
-    cart.push(array)
-    console.log(cart)
-    this.count = count;
-  }
+
+            articleInCart = [];
+            modalCart.innerHTML = "";
+            Object.keys(localStorage).forEach(function(key){
+                currentItem = JSON.parse(localStorage.getItem(key));
+                productName = currentItem[0];
+                productPrice = currentItem[1];
+                productRef = currentItem[2];
+                productQuantity = currentItem[3];
+                let text = `<div>Nom : ${productName} , Prix = ${productPrice}, Quantité : ${productQuantity}</div>`
+                articleInCart.push(text)
+            });
+            articleInCart.forEach(function(element){
+                modalCart.innerHTML += element;
+            })
+        }
+    });
+}
+
+
 
 
 
@@ -69,10 +109,50 @@ function addToCart(name, id, price, count) {
 
 
 // for (let i = 0; i < btnAddToCart.length; i++) {
-//     btnAddToCart[i].addEventListener('click', function () {
-//         console.log("test");
-//     });
+// btnAddToCart[i].addEventListener('click', Item(name, id, price, count));
+// }
+//         })
+// })
+// let refls = localStorage.getItem('refls')
+// let cart = []
+
+// function Item(name, id, price, count) {
+//     let array = JSON.parse(localStorage.getItem('cart'));
+//     let product = this.dataset.product;
+//     let ref = this.dataset.ref
+//     let productPrice = this.dataset.price;
+//     this.count = count;
+//     array.push(ref);
+//     JSON.stringify(localStorage.setItem('cart'));
+
+
+// let array = []
+// array.push(product,ref,productPrice)
+// cart.push(array)
+// console.log(cart)
 // }
 
 
+// function addToCart(){
+//     let array = []
+//     array.push(Item)
+//     cart.push(array)
+//     console.log(cart)
+// }
 
+
+// function clearCart(){
+//     cart = []
+// }
+
+// array.push(product, ref, productPrice)
+// cart.push(array)
+// console.log(cart)
+
+
+
+// for(let item in cart) {
+//     if(cart[item].product === product) {
+//       cart[item].count ++;
+//     }
+// }
